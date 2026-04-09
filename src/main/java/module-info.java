8c92251dad4a959832863c3f8ece97e70fcdea6a
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-04-09 14:03:55 +0200

    feat(service): [Add] Method getAllStudents - FD

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,15 @@
 module com.tracker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires io.github.cdimascio.dotenv.java;
 
     opens com.tracker to javafx.fxml;
+    opens com.tracker.controller to javafx.fxml;
+    opens com.tracker.model to javafx.base;
+
     exports com.tracker;
+    exports com.tracker.controller;
+    exports com.tracker.model;
+    exports com.tracker.service;
 }
