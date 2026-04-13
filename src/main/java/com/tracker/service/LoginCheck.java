ab447666ad7a61433a84048352de8a3d3ae5a36d
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-04-13 09:34:16 +0200

    feat(service): checkpoint / stand-by for merge - FD

--- src/main/java/com/tracker/service/LoginCheck.java
+++ src/main/java/com/tracker/service/LoginCheck.java
@@ -1,5 +1,7 @@
 package com.tracker.service;
 
 public class LoginCheck {
-    
+    public boolean checkLogin(String username, String password) {
+        
+    }
 }
--- src/main/java/com/tracker/service/StudentService.java
+++ src/main/java/com/tracker/service/StudentService.java
@@ -27,28 +27,35 @@ public class StudentService {
             String direction = ascending ? "ASC" : "DESC";
 
 
+
+
             // Switch for sort column
             String colSql;
             switch (sortBy.toLowerCase()) {
                 case "id":
-                    colSql = "last_name";
+                    colSql = "id";
                     break;
                 case "first_name":
+                case "prenom":
                     colSql = "first_name";
                     break;
                 case "last_name":
+                case "nom":
                     colSql = "last_name";
                     break;
                 case "age":
                     colSql = "age";
                     break;
                 case "grade":
+                case "moyenne":
                     colSql = "grade";
                     break;
                 default:
-                    colSql = "last_name";
+                    colSql = "id";
                     break;
             }
+
+
 
             // SQL request
             String request = "SELECT * FROM \"Student\" ORDER BY " + colSql + " " + direction;
@@ -80,15 +87,46 @@ public class StudentService {
 
 
 
-    public void addStudent(){
+
+
+
+    public List<Student> searchStudents(String search) {
+        List<Student> studentsList = new ArrayList<>();
+
+        String pattern = "%" + search + "%";
         
+        String query = "SELECT * FROM \"Student\" WHERE " +
+                    "first_name LIKE ? OR " +
+                    "last_name LIKE ? OR " +
+                    "CAST(id AS TEXT) LIKE ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+
+            pstmt.setString(1, pattern);
+            pstmt.setString(2, pattern);
+            pstmt.setString(3, pattern);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Student s = new Student();
+                s.setId(rs.getInt("id"));
+                s.setFirstName(rs.getString("first_name"));
+                s.setLastName(rs.getString("last_name"));
+                s.setAge(rs.getInt("age"));
+                s.setAverage(rs.getDouble("grade"));
+                studentsList.add(s);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur recherche : " + e.getMessage());
+        }
+        return studentsList;
     }
-    public void deleteStudent(){
-        
-    }
-    public void updateStudent(){
-        
-    }
-    public void getStudent(){
-    }
+
+    public void addStudent() {}
+    public void deleteStudent() {}
+    public void updateStudent() {}
+    public void getStudent() {}
 }
