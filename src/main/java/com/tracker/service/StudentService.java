7236f064fa6312b3a28a95ee1bba36eff44d4b3b
florian-d-agostino <florian.d-agostino@laplateforme.io>
2026-04-14 10:20:55 +0200

    feat(service) Merge Student service in preprod - FD

---
+++ src/main/java/com/tracker/service/StudentService.java
@@ -0,0 +1,229 @@
+package com.tracker.service;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.tracker.config.DatabaseConfig;
+import com.tracker.model.Student;
+
+
+
+
+
+
+
+
+
+
+public class StudentService {
+
+    // -------- Get All Student --------
+    public List<Student> getAllStudents(String sortBy, boolean ascending){
+        List<Student> studentsList = new ArrayList<>();
+        try {
+
+            // Database connection
+            Connection connection = DatabaseConfig.getConnection();
+
+            // Sort direction
+            String direction = ascending ? "ASC" : "DESC";
+
+
+
+
+            // Switch secure and collumn
+            String colSql;
+            switch (sortBy.toLowerCase()) {
+                case "id": colSql = "id";
+                    break;
+
+                case "first_name":
+                case "prenom": colSql = "first_name";
+                    break;
+
+                case "last_name":
+                case "nom": colSql = "last_name";
+                    break;
+
+                case "age": colSql = "age";
+                    break;
+
+                case "grade":
+                case "moyenne": colSql = "grade";
+                    break;
+
+                default: colSql = "id";
+                    break;
+            }
+
+            
+
+            // SQL request
+            String request = "SELECT * FROM \"Student\" ORDER BY " + colSql + " " + direction;
+
+            // Request execution
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(request);
+
+
+
+
+            // Student data extraction
+            while(resultSet.next()) {
+                Student student = new Student();
+                student.setId(resultSet.getInt("id"));
+                student.setFirstName(resultSet.getString("first_name"));
+                student.setLastName(resultSet.getString("last_name"));
+                student.setAge(resultSet.getInt("age"));
+                student.setAverage(resultSet.getDouble("grade"));
+
+                studentsList.add(student);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Error while retrieving students: " + e.getMessage());
+        }
+        return studentsList;
+        
+    }
+
+
+
+
+
+    // -------- Search Student --------
+    public List<Student> searchStudents(String search) {
+        List<Student> studentsList = new ArrayList<>();
+
+        String pattern = "%" + search + "%";
+        
+        String query = "SELECT * FROM \"Student\" WHERE " +
+                    "first_name LIKE ? OR " +
+                    "last_name LIKE ? OR " +
+                    "CAST(id AS TEXT) LIKE ?";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query)) {
+            
+
+            pstmt.setString(1, pattern);
+            pstmt.setString(2, pattern);
+            pstmt.setString(3, pattern);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Student s = new Student();
+                s.setId(rs.getInt("id"));
+                s.setFirstName(rs.getString("first_name"));
+                s.setLastName(rs.getString("last_name"));
+                s.setAge(rs.getInt("age"));
+                s.setAverage(rs.getDouble("grade"));
+                studentsList.add(s);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur recherche : " + e.getMessage());
+        }
+        return studentsList;
+    }
+
+
+
+
+
+// -------- Add Student --------
+    public void addStudent(Student student) {
+        String request = "INSERT INTO \"Student\" (first_name, last_name, age, grade) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(request)) {
+            
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getLastName());
+            preparedStatement.setInt(3, student.getAge());
+            preparedStatement.setDouble(4, student.getAverage());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error while adding student: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+
+    // -------- Delete Student --------
+    public void deleteStudent(Student student) {
+        String request = "DELETE FROM \"Student\" WHERE id = ?";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(request)) {
+            
+            preparedStatement.setInt(1, student.getId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error while deleting student: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+    // -------- Update Student --------
+    public void updateStudent(Student student) {
+        String request = "UPDATE \"Student\" SET first_name = ?, last_name = ?, age = ?, grade = ? WHERE id = ?";
+
+        try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(request)) {
+
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getLastName());
+            preparedStatement.setInt(3, student.getAge());
+            preparedStatement.setDouble(4, student.getAverage());
+            preparedStatement.setInt(5, student.getId());
+
+            preparedStatement.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.err.println("Error while updating student: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+    // -------- Get Student --------
+    public Student getStudent(int id) { 
+    String request = "SELECT * FROM \"Student\" WHERE id = ?";
+    Student student = null;
+
+    try (Connection connection = DatabaseConfig.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(request)) {
+
+        preparedStatement.setInt(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            student = new Student();
+            student.setId(resultSet.getInt("id"));
+            student.setFirstName(resultSet.getString("first_name"));
+            student.setLastName(resultSet.getString("last_name"));
+            student.setAge(resultSet.getInt("age"));
+            student.setAverage(resultSet.getDouble("grade"));
+        }
+
+            } catch (SQLException e) {
+            System.err.println("Error while retrieving student: " + e.  getMessage());
+            }
+
+        return student;
+    }
+
+}
